d85cee8e635b6c75646ddbdc6f37d63a7c148f44
Nandhakumar <nandhanaveengasc41@gmail.com>
2026-02-25 22:58:25 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,24 +1,11 @@
 <?php
 date_default_timezone_set('UTC');
 
-// If running on Railway
-if (getenv('MYSQLHOST')) {
-
-    $host = getenv('MYSQLHOST');
-    $user = getenv('MYSQLUSER');
-    $pass = getenv('MYSQLPASSWORD');
-    $db   = getenv('MYSQLDATABASE');
-    $port = getenv('MYSQLPORT');
-
-} else {
-
-    // Local XAMPP
-    $host = "127.0.0.1";
-    $user = "root";
-    $pass = "";
-    $db   = "intercollege_meet_app";
-    $port = 3307;
-}
+$host = getenv('DB_HOST') ?: "127.0.0.1";
+$user = getenv('DB_USER') ?: "root";
+$pass = getenv('DB_PASS') ?: "";
+$db   = getenv('DB_NAME') ?: "intercollege_meet_app";
+$port = getenv('DB_PORT') ?: 3307;
 
 $conn = new mysqli($host, $user, $pass, $db, (int)$port);
 
