b68bf1ce9635067462f2dce52cf826ca268ba1e6
Nandhakumar <nandhanaveengasc41@gmail.com>
2026-02-25 23:11:23 +0530

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,25 +1,19 @@
 <?php
 date_default_timezone_set('UTC');
 
-if (getenv('DATABASE_URL')) {
+$url = getenv('DATABASE_URL');
 
-    $url = parse_url(getenv('DATABASE_URL'));
-
-    $host = $url['host'];
-    $user = $url['user'];
-    $pass = $url['pass'];
-    $db   = ltrim($url['path'], '/');
-    $port = $url['port'];
-
-} else {
-
-    // Local XAMPP fallback
-    $host = "127.0.0.1";
-    $user = "root";
-    $pass = "";
-    $db   = "intercollege_meet_app";
-    $port = 3307;
+if (!$url) {
+    die("DATABASE_URL not found.");
 }
+
+$dbparts = parse_url($url);
+
+$host = $dbparts['host'];
+$user = $dbparts['user'];
+$pass = $dbparts['pass'];
+$db   = ltrim($dbparts['path'], '/');
+$port = $dbparts['port'];
 
 $conn = new mysqli($host, $user, $pass, $db, (int)$port);
 
@@ -27,7 +21,5 @@ if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+session_start();
 ?>
